02eafac4491e70d081b35c4155d3ad29d4468766
mrseay24 <sidneyeseay@gmail.com>
2022-05-16 01:23:26 -0400

    Add files via upload

---
+++ HelloSidney.java
@@ -0,0 +1,10 @@
+package helloSidney;
+
+public class HelloSidney {
+	public static void main(String [] args) {
+		System.out.println("Hello Sidney!");
+		System.out.print("You ");
+		System.out.print("are ");
+		System.out.print("sexy!");
+	}
+}
